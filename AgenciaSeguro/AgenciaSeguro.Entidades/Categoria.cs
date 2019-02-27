6f68fe8c5e4ef57bcaabec61e60906b1aed8da92
luifer227 <luisjimenezh7@gmail.com>
2019-02-27 10:04:02 -0500

    Primera carga de archivos, creacion de los distintos proyectos y la entidad Categoria

---
+++ AgenciaSeguro/AgenciaSeguro.Entidades/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgenciaSeguro.Entidades
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "El nombre no debe de tener más de 50 caracteres, ni menos de 10 caracteres.")]
+        public string Nombre { get; set; }
+        [StringLength(256)]
+        public string Descripcion { get; set; }
+       
+    }
+}
